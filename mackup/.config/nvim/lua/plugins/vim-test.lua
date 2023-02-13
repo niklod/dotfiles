d260e9340d8cbb731d829c2f60d147960398f778
Nikita Lodyanoy <nikita@mobalyticshq.com>
2023-02-13 09:54:15 +0300

    update

--- mackup/.config/nvim/lua/plugins/vim-test.lua
+++ mackup/.config/nvim/lua/plugins/vim-test.lua
@@ -1,3 +1,7 @@
 return {
 	"vim-test/vim-test",
+	keys = {
+		{ "<Leader>tr", "<cmd>TestNearest -strategy=vimux -v<cr>", desc = "Run nearest test", remap = true },
+		{ "<Leader>tl", "<cmd>TestLast -strategy=vimux -v<cr>", desc = "Run last test", remap = true },
+	},
 }
