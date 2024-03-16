dd950cfad95f8dc00831d28516212f3a898fe385
Nikita Lodyanoy <nikita@mobalyticshq.com>
2024-03-16 13:00:35 +0300

    update

--- mackup/.config/nvim/lua/plugins/nvim-dap.lua
+++ mackup/.config/nvim/lua/plugins/nvim-dap.lua
@@ -42,65 +42,6 @@ return {
 			local dap = require("dap")
 			require("dap.ext.vscode").load_launchjs()
 
-			-- Placeholder expansion for launch directives
-			local placeholders = {
-				["${file}"] = function(_)
-					return vim.fn.expand("%:p")
-				end,
-				["${fileBasename}"] = function(_)
-					return vim.fn.expand("%:t")
-				end,
-				["${fileBasenameNoExtension}"] = function(_)
-					return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r")
-				end,
-				["${fileDirname}"] = function(_)
-					return vim.fn.expand("%:p:h")
-				end,
-				["${fileExtname}"] = function(_)
-					return vim.fn.expand("%:e")
-				end,
-				["${relativeFile}"] = function(_)
-					return vim.fn.expand("%:.")
-				end,
-				["${relativeFileDirname}"] = function(_)
-					return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
-				end,
-				["${workspaceFolder}"] = function(_)
-					return vim.fn.getcwd()
-				end,
-				["${workspaceFolderBasename}"] = function(_)
-					return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-				end,
-				["${env:([%w_]+)}"] = function(match)
-					return os.getenv(match) or ""
-				end,
-			}
-			for type, _ in pairs(dap.configurations) do
-				for _, config in pairs(dap.configurations[type]) do
-					if config.envFile then
-						local filePath = config.envFile
-						for key, fn in pairs(placeholders) do
-							filePath = filePath:gsub(key, fn)
-						end
-
-						-- Verify file exists
-						local f = io.open(filePath, "r")
-						if f ~= nil then
-							for line in io.lines(filePath) do
-								local words = {}
-								for word in string.gmatch(line, "[^=]+") do
-									table.insert(words, all_trim(word))
-								end
-								if not config.env then
-									config.env = {}
-								end
-								config.env[words[1]] = words[2]
-							end
-						end
-					end
-				end
-			end
-
 			dap.adapters.go = {
 				type = "server",
 				port = 38697,
@@ -108,6 +49,63 @@ return {
 					command = "dlv",
 					args = { "dap", "-l", "127.0.0.1:38697" },
 				},
+				enrich_config = function(finalConfig, on_config)
+					local final_config = vim.deepcopy(finalConfig)
+
+					-- Placeholder expansion for launch directives
+					local placeholders = {
+						["${file}"] = function(_)
+							return vim.fn.expand("%:p")
+						end,
+						["${fileBasename}"] = function(_)
+							return vim.fn.expand("%:t")
+						end,
+						["${fileBasenameNoExtension}"] = function(_)
+							return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r")
+						end,
+						["${fileDirname}"] = function(_)
+							return vim.fn.expand("%:p:h")
+						end,
+						["${fileExtname}"] = function(_)
+							return vim.fn.expand("%:e")
+						end,
+						["${relativeFile}"] = function(_)
+							return vim.fn.expand("%:.")
+						end,
+						["${relativeFileDirname}"] = function(_)
+							return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
+						end,
+						["${workspaceFolder}"] = function(_)
+							return vim.fn.getcwd()
+						end,
+						["${workspaceFolderBasename}"] = function(_)
+							return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+						end,
+						["${env:([%w_]+)}"] = function(match)
+							return os.getenv(match) or ""
+						end,
+					}
+
+					if final_config.envFile then
+						local filePath = final_config.envFile
+						for key, fn in pairs(placeholders) do
+							filePath = filePath:gsub(key, fn)
+						end
+
+						for line in io.lines(filePath) do
+							local words = {}
+							for word in string.gmatch(line, "[^=]+") do
+								table.insert(words, word)
+							end
+							if not final_config.env then
+								final_config.env = {}
+							end
+							final_config.env[words[1]] = words[2]
+						end
+					end
+
+					on_config(final_config)
+				end,
 			}
 		end,
 	},
